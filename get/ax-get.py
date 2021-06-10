5d02499c3c2f62fd4b409402e40e9c6f18813a33
Richard J <techpowerawaits@outlook.com>
2021-06-09 20:19:27 -0600

    Format python scripts with Black
    Reduces line length and introduces a standard format.

--- get/ax-get.py
+++ get/ax-get.py
@@ -5,6 +5,7 @@ import argparse
 import os
 import requests
 import shutil
+
 # So the instructions have a bit
 # of a breather between them.
 import time
@@ -21,15 +22,23 @@ with open(ver_path, "r") as version_file:
 # Source code wise, Axelor Open Suite depends on Axelor Open Webapp.
 # Each url should provide binary WAR files with priority given to
 # Open Suite.
-default_opensuite_src_url = "https://github.com/axelor/axelor-open-suite/archive/refs/tags/v"
-default_opensuite_war_url = "https://github.com/axelor/axelor-open-suite/releases/download/v"
-default_openwebapp_src_url = "https://github.com/axelor/open-suite-webapp/archive/refs/tags/v"
-default_openwebapp_war_url = "https://github.com/axelor/open-suite-webapp/releases/download/v"
+default_opensuite_src_url = (
+    "https://github.com/axelor/axelor-open-suite/archive/refs/tags/v"
+)
+default_opensuite_war_url = (
+    "https://github.com/axelor/axelor-open-suite/releases/download/v"
+)
+default_openwebapp_src_url = (
+    "https://github.com/axelor/open-suite-webapp/archive/refs/tags/v"
+)
+default_openwebapp_war_url = (
+    "https://github.com/axelor/open-suite-webapp/releases/download/v"
+)
 default_war_basename = "axelor-erp-v"
 
 parser = argparse.ArgumentParser(
-    description="Grabs a copy of Axelor",
-    epilog="Licensed under the 0BSD.")
+    description="Grabs a copy of Axelor", epilog="Licensed under the 0BSD."
+)
 parser.add_argument("-v", "--version", action="version", version=ver_str)
 parser.add_argument("-s", "--src", action="store_true")
 parser.add_argument("major", help="Major version number")
@@ -40,14 +49,28 @@ is_src = parser_args.src
 major_ver = parser_args.major
 minor_ver = parser_args.minor
 patch_ver = parser_args.patch
-ax_ver_str = major_ver + '.' + minor_ver + '.' + patch_ver
+ax_ver_str = major_ver + "." + minor_ver + "." + patch_ver
 
 opensuite_src_url = default_opensuite_src_url + ax_ver_str + ".zip"
-opensuite_war_url = default_opensuite_war_url + ax_ver_str + '/' + default_war_basename + ax_ver_str + ".war"
+opensuite_war_url = (
+    default_opensuite_war_url
+    + ax_ver_str
+    + "/"
+    + default_war_basename
+    + ax_ver_str
+    + ".war"
+)
 opensuite_src_zip_name = "opensuite_src.zip"
 opensuite_src_folder_name = "axelor-open-suite-" + ax_ver_str
 openwebapp_src_url = default_openwebapp_src_url + ax_ver_str + ".zip"
-openwebapp_war_url = default_openwebapp_war_url + ax_ver_str + '/' + default_war_basename + ax_ver_str + ".war"
+openwebapp_war_url = (
+    default_openwebapp_war_url
+    + ax_ver_str
+    + "/"
+    + default_war_basename
+    + ax_ver_str
+    + ".war"
+)
 openwebapp_src_folder_name = "open-suite-webapp-" + ax_ver_str
 openwebapp_src_zip_name = "openwebapp_src.zip"
 src_name = "axelor-v" + ax_ver_str + "-src"
@@ -55,7 +78,7 @@ war_name = "axelor-v" + ax_ver_str + ".war"
 war_folder_name = "axelor-v" + ax_ver_str
 
 # Path to application.properties
-app_prop_path = ''
+app_prop_path = ""
 
 if is_src:
     webapp_src = requests.get(openwebapp_src_url)
@@ -69,22 +92,37 @@ if is_src:
         for content in opensuite_src.iter_content(chunk_size=40):
             opensuite_src_fp.write(content)
     with zipfile.ZipFile(opensuite_src_zip_name, "r") as opensuite_src_zip:
-        opensuite_src_dest = os.path.join(current_path, openwebapp_src_folder_name, "modules", "axelor-open-suite")
+        opensuite_src_dest = os.path.join(
+            current_path, openwebapp_src_folder_name, "modules", "axelor-open-suite"
+        )
         opensuite_src_zip.extractall(os.path.dirname(opensuite_src_dest))
         if os.path.exists(opensuite_src_dest):
             shutil.rmtree(opensuite_src_dest)
-        os.rename(os.path.join(os.path.dirname(opensuite_src_dest), opensuite_src_folder_name), opensuite_src_dest)
+        os.rename(
+            os.path.join(
+                os.path.dirname(opensuite_src_dest), opensuite_src_folder_name
+            ),
+            opensuite_src_dest,
+        )
     os.remove(openwebapp_src_zip_name)
     os.remove(opensuite_src_zip_name)
     os.rename(openwebapp_src_folder_name, src_name)
-    app_prop_path = os.path.join(src_name, "src", "main", "resources", "application.properties")
+    app_prop_path = os.path.join(
+        src_name, "src", "main", "resources", "application.properties"
+    )
     if os.path.exists("branding_logo.png"):
-        brand_path = os.path.join(src_name, "src", "main", "webapp", "img", "branding_logo.png")
+        brand_path = os.path.join(
+            src_name, "src", "main", "webapp", "img", "branding_logo.png"
+        )
         shutil.copyfile("branding_logo.png", brand_path)
-        print("A personalized logo has been copied to " + brand_path + '.')
-        print("Please edit the \"application.logo\" entry in " + app_prop_path + " to apply this new logo.")
-        print("Typically, the entry will be set (by default) to \"img/axelor.png\".")
-        print("Simply change this to \"img/branding_logo.png\".")
+        print("A personalized logo has been copied to " + brand_path + ".")
+        print(
+            'Please edit the "application.logo" entry in '
+            + app_prop_path
+            + " to apply this new logo."
+        )
+        print('Typically, the entry will be set (by default) to "img/axelor.png".')
+        print('Simply change this to "img/branding_logo.png".')
         print()
         time.sleep(PAUSE_SECONDS)
 else:
@@ -102,31 +140,57 @@ else:
         for member in war_zip.infolist():
             war_zip.extract(member, war_folder_name)
     os.remove(war_name)
-    app_prop_path = os.path.join(war_folder_name, "WEB-INF", "classes", "application.properties")
+    app_prop_path = os.path.join(
+        war_folder_name, "WEB-INF", "classes", "application.properties"
+    )
     if os.path.exists("branding_logo.png"):
         brand_path = os.path.join(war_folder_name, "img", "branding_logo.png")
         shutil.copyfile("branding_logo.png", brand_path)
-        print("A personalized logo has been copied to " + brand_path + '.')
-        print("Please edit the \"application.logo\" entry in " + app_prop_path + " to apply this new logo.")
-        print("Typically, the entry will be set (by default) to \"img/axelor.png\".")
-        print("Simply change this to \"img/branding_logo.png\".")
+        print("A personalized logo has been copied to " + brand_path + ".")
+        print(
+            'Please edit the "application.logo" entry in '
+            + app_prop_path
+            + " to apply this new logo."
+        )
+        print('Typically, the entry will be set (by default) to "img/axelor.png".')
+        print('Simply change this to "img/branding_logo.png".')
         print()
         time.sleep(PAUSE_SECONDS)
 
-print("In order to get Axelor working, the " + app_prop_path + " file needs various database-related changes.")
+print(
+    "In order to get Axelor working, the "
+    + app_prop_path
+    + " file needs various database-related changes."
+)
 print()
 time.sleep(PAUSE_SECONDS)
-print("More specifically, the name of the database its going to use and the db user account info that owns the database needs to be entered.")
-print("This script does not generate a database for you, but please ensure that no databases used for this version were used by previous versions of Axelor.")
+print(
+    "More specifically, the name of the database its going to use and the db user account info that owns the database needs to be entered."
+)
+print(
+    "This script does not generate a database for you, but please ensure that no databases used for this version were used by previous versions of Axelor."
+)
 print("This is to ensure a more reliable experience.")
-print("To copy all the information from a previous instance of Axelor, please use the its built-in backup and restore feature.")
+print(
+    "To copy all the information from a previous instance of Axelor, please use the its built-in backup and restore feature."
+)
 print()
 time.sleep(PAUSE_SECONDS)
-print("If a database hasn't been created already, it is recommended to name it after the specific version number you are running.")
-print("For example, axelor-"+ ax_ver_str + " would make a great database name.")
-print("This will make life easier in case it is necessary to revert to a previous version.")
-print("In that case, there would be no need to worry about conflict due to a newer version of Axelor updating the database.")
+print(
+    "If a database hasn't been created already, it is recommended to name it after the specific version number you are running."
+)
+print("For example, axelor-" + ax_ver_str + " would make a great database name.")
+print(
+    "This will make life easier in case it is necessary to revert to a previous version."
+)
+print(
+    "In that case, there would be no need to worry about conflict due to a newer version of Axelor updating the database."
+)
 if not is_src:
     time.sleep(PAUSE_SECONDS)
     print()
-    print("In order to avoid having to specify " + war_folder_name + " while typing in the URL or IP Address to access Axelor, please rename the folder to \"ROOT\".")
+    print(
+        "In order to avoid having to specify "
+        + war_folder_name
+        + ' while typing in the URL or IP Address to access Axelor, please rename the folder to "ROOT".'
+    )
--- invconv/ax-invconv.py
+++ invconv/ax-invconv.py
@@ -33,133 +33,153 @@ AXELOR_CSV_COLUMNS = [
     "productCategory_importId",
     "saleSupplySelect",
     "fullName",
-    "saleCurrency_importId"
+    "saleCurrency_importId",
 ]
 AXELOR_FAMILY_SHORTHAND = {
-    "Components" : "COMP",
-    "Consumables" : "CONS",
-    "Equipment" : "EQPT",
-    "Expenses" : "TEXP",
-    "Services" : "SERV",
-    "Subscription" : "HSGT"
+    "Components": "COMP",
+    "Consumables": "CONS",
+    "Equipment": "EQPT",
+    "Expenses": "TEXP",
+    "Services": "SERV",
+    "Subscription": "HSGT",
 }
 AXELOR_PRODUCT_CATEGORIES = {
-    "Accomodation" : 16,
-    "Case" : 7,
-    "Hard Disk" : 5,
-    "Hosting" : 1,
-    "Maintenance" : 2,
-    "Meal" : 15,
-    "Memory" : 8,
-    "Motherboard" : 9,
-    "Non-perishable" : 10,
-    "Package" : 18,
-    "Perishable" : 11,
-    "Printer" : 4,
-    "Processor" : 6,
+    "Accomodation": 16,
+    "Case": 7,
+    "Hard Disk": 5,
+    "Hosting": 1,
+    "Maintenance": 2,
+    "Meal": 15,
+    "Memory": 8,
+    "Motherboard": 9,
+    "Non-perishable": 10,
+    "Package": 18,
+    "Perishable": 11,
+    "Printer": 4,
+    "Processor": 6,
     "Project based": 13,
-    "Screen" : 17,
-    "Server" : 3,
-    "Time based" : 12,
-    "Transport" : 14
+    "Screen": 17,
+    "Server": 3,
+    "Time based": 12,
+    "Transport": 14,
 }
 AXELOR_CATEGORY_SHORTHAND = {
-    "Accomodation" : "ACCO",
-    "Case" : "CASE",
-    "Hard Disk" : "HDD",
-    "Hosting" : "HOST",
-    "Maintenance" : "MAIN",
-    "Meal" : "MEAL",
-    "Memory" : "MEM",
-    "Motherboard" : "MOB",
-    "Non-perishable" : "NPER",
-    "Package" : "PACK",
-    "Perishable" : "PER",
-    "Print" : "PTR",
-    "Processor" : "PROC",
-    "Project based" : "PROB",
-    "Screen" : "SCR",
-    "Server" : "SER",
-    "Time based" : "TIMB",
-    "Transport" : "TRAN"
+    "Accomodation": "ACCO",
+    "Case": "CASE",
+    "Hard Disk": "HDD",
+    "Hosting": "HOST",
+    "Maintenance": "MAIN",
+    "Meal": "MEAL",
+    "Memory": "MEM",
+    "Motherboard": "MOB",
+    "Non-perishable": "NPER",
+    "Package": "PACK",
+    "Perishable": "PER",
+    "Print": "PTR",
+    "Processor": "PROC",
+    "Project based": "PROB",
+    "Screen": "SCR",
+    "Server": "SER",
+    "Time based": "TIMB",
+    "Transport": "TRAN",
 }
 AXELOR_PRODUCT_FAMILIES = {
-    "Components" : 3,
-    "Consumables" : 4,
-    "Equipment" : 2,
-    "Expenses" : 6,
-    "Services" : 5,
-    "Subscription" : 1
+    "Components": 3,
+    "Consumables": 4,
+    "Equipment": 2,
+    "Expenses": 6,
+    "Services": 5,
+    "Subscription": 1,
 }
-AXELOR_PRODUCT_TYPE = [
-    "Product",
-    "Service"
-]
+AXELOR_PRODUCT_TYPE = ["Product", "Service"]
 AXELOR_UNITS = {
-    "Box of 1000 Pces" : 10,
-    "Box of 100 Pces" : 8,
-    "Box of 12 Pces" : 5,
-    "Box of 2 Pces" : 3,
-    "Box of 500 Pces" : 9,
-    "Box of 50 Pces" : 7,
-    "Box of 6 Pces" : 4,
-    "Centigrams" : 20,
-    "Centimeter" : 29,
-    "Day" : 14,
-    "Decagrams" : 23,
-    "Decameter" : 32,
-    "Decigrams" : 21,
-    "Grams" : 22,
-    "Half-Day" : 13,
-    "Hectograms" : 24,
-    "Hectometer" : 33,
-    "Hour" : 12,
-    "Kilograms" : 25,
-    "Kilometer" : 34,
-    "Meter" : 31,
-    "Miles" : 35,
-    "Milligrams" : 19,
-    "Millimeter" : 28,
-    "Minute" : 11,
-    "Month" : 16,
-    "Percent" : 18,
-    "Piece" : 2,
-    "Quintal" : 26,
-    "Ton" : 27,
-    "Unit" : 1,
-    "Week" : 15,
-    "Year" : 17
+    "Box of 1000 Pces": 10,
+    "Box of 100 Pces": 8,
+    "Box of 12 Pces": 5,
+    "Box of 2 Pces": 3,
+    "Box of 500 Pces": 9,
+    "Box of 50 Pces": 7,
+    "Box of 6 Pces": 4,
+    "Centigrams": 20,
+    "Centimeter": 29,
+    "Day": 14,
+    "Decagrams": 23,
+    "Decameter": 32,
+    "Decigrams": 21,
+    "Grams": 22,
+    "Half-Day": 13,
+    "Hectograms": 24,
+    "Hectometer": 33,
+    "Hour": 12,
+    "Kilograms": 25,
+    "Kilometer": 34,
+    "Meter": 31,
+    "Miles": 35,
+    "Milligrams": 19,
+    "Millimeter": 28,
+    "Minute": 11,
+    "Month": 16,
+    "Percent": 18,
+    "Piece": 2,
+    "Quintal": 26,
+    "Ton": 27,
+    "Unit": 1,
+    "Week": 15,
+    "Year": 17,
 }
 AXELOR_UNITS_SHORTHAND = {
-    "Centigrams" : "cg",
-    "Centimeter" : "cm",
-    "Decagrams" : "dag",
-    "Decameter" : "dam",
-    "Grams" : "g",
-    "Hectograms" : "hg",
-    "Hectometer" : "hm",
-    "Hour" : "hr",
-    "Kilograms" : "kg",
-    "Kilometer" : "km",
-    "Minute" : "min",
-    "Percent" : '%',
-    "Piece" : "pce",
-    "Ton" : "t",
-    "Unit" : "u",
-    "Year" : "a"
+    "Centigrams": "cg",
+    "Centimeter": "cm",
+    "Decagrams": "dag",
+    "Decameter": "dam",
+    "Grams": "g",
+    "Hectograms": "hg",
+    "Hectometer": "hm",
+    "Hour": "hr",
+    "Kilograms": "kg",
+    "Kilometer": "km",
+    "Minute": "min",
+    "Percent": "%",
+    "Piece": "pce",
+    "Ton": "t",
+    "Unit": "u",
+    "Year": "a",
 }
 MAX_UNIT_SHORTHAND = 3
 
 parser = argparse.ArgumentParser(
     description="Converts inventory lists to a Axelor-compatible CSV format",
-    epilog="Licensed under the 0BSD.")
-parser.add_argument("-t", "--type", default="xslx",
+    epilog="Licensed under the 0BSD.",
+)
+parser.add_argument(
+    "-t",
+    "--type",
+    default="xslx",
     choices=["xslx"],
-    help="The type of file that is being imported")
+    help="The type of file that is being imported",
+)
 parser.add_argument("-v", "--version", action="version", version=ver_str)
-parser.add_argument("-c", "--category", choices=AXELOR_PRODUCT_CATEGORIES, default="Package", help="Default product category to place in output")
-parser.add_argument("-f", "--family", choices=AXELOR_PRODUCT_FAMILIES, default="Equipment", help="Default product family to place in output")
-parser.add_argument("-T", "--Type", choices=AXELOR_PRODUCT_TYPE, default="Product", help="Default product type to place in output")
+parser.add_argument(
+    "-c",
+    "--category",
+    choices=AXELOR_PRODUCT_CATEGORIES,
+    default="Package",
+    help="Default product category to place in output",
+)
+parser.add_argument(
+    "-f",
+    "--family",
+    choices=AXELOR_PRODUCT_FAMILIES,
+    default="Equipment",
+    help="Default product family to place in output",
+)
+parser.add_argument(
+    "-T",
+    "--Type",
+    choices=AXELOR_PRODUCT_TYPE,
+    default="Product",
+    help="Default product type to place in output",
+)
 parser.add_argument("input", help="Input file")
 parser_args = parser.parse_args()
 input_file = parser_args.input
@@ -168,22 +188,19 @@ default_family = parser_args.family
 default_type = parser_args.Type
 
 xslx_file = load_workbook(
-    input_file,
-    read_only=True,
-    keep_vba=False,
-    data_only=True,
-    keep_links=False)
+    input_file, read_only=True, keep_vba=False, data_only=True, keep_links=False
+)
 
 csv_out = csv.writer(sys.stdout, dialect="excel")
 csv_out.writerow(AXELOR_CSV_COLUMNS)
 
 # This is used to populate the CSV file.
 xslx_col_index = {
-    "description" : -1,
-    "name" : -1,
-    "category" : -1,
-    "sales_UOM" : -1,
-    "purchase_UOM" : -1
+    "description": -1,
+    "name": -1,
+    "category": -1,
+    "sales_UOM": -1,
+    "purchase_UOM": -1,
 }
 xslx_headers = []
 
@@ -200,7 +217,7 @@ for row in xslx_file.active.iter_rows(
     max_row=xslx_file.active.max_row,
     min_col=start_title_col,
     max_col=end_title_col,
-    values_only=True
+    values_only=True,
 ):
     for cell in row:
         if cell is None:
@@ -215,31 +232,42 @@ for row in xslx_file.active.iter_rows(
     max_row=xslx_header_row,
     min_col=xslx_file.active.min_column,
     max_col=xslx_file.active.max_column,
-    values_only=True
+    values_only=True,
 ):
     for cell in row:
         xslx_headers += [cell]
 # col and row are indexed starting at 1.
 header_index = 1
 for header in xslx_headers:
-    if (header.lower().find("descript") != -1) and (xslx_col_index["description"] == -1):
+    if (header.lower().find("descript") != -1) and (
+        xslx_col_index["description"] == -1
+    ):
         xslx_col_index["description"] = header_index
     # The name would be one of the first columns in a file.
     # In case a file has a header containing "name" and another
     # containing "ID", it should still have the correct one.
-    elif ((header.lower().find("name") != -1) or
-    (header.lower().find("id") != -1)) and (xslx_col_index["name"] == -1):
+    elif (
+        (header.lower().find("name") != -1) or (header.lower().find("id") != -1)
+    ) and (xslx_col_index["name"] == -1):
         xslx_col_index["name"] = header_index
     elif (header.lower().find("cat") != -1) and (xslx_col_index["category"] == -1):
         xslx_col_index["category"] = header_index
-    elif ((header.lower().find("unit") != -1) or
-    (header.lower().find("uom") != -1)) and (xslx_col_index["sales_UOM"] == -1):
+    elif (
+        (header.lower().find("unit") != -1) or (header.lower().find("uom") != -1)
+    ) and (xslx_col_index["sales_UOM"] == -1):
         xslx_col_index["sales_UOM"] = header_index
-    elif (header.lower().find("unit") != -1) and ((header.lower().find("package") != -1) or header.lower().find("pkg") != -1) and (xslx_col_index["purchase_UOM"] == -1):
+    elif (
+        (header.lower().find("unit") != -1)
+        and ((header.lower().find("package") != -1) or header.lower().find("pkg") != -1)
+        and (xslx_col_index["purchase_UOM"] == -1)
+    ):
         xslx_col_index["purchase_UOM"] = header_index
     else:
-        print("Warning: column " + '"' + header + '"' " from " + input_file +
-        " will be ignored.", file=sys.stderr)
+        print(
+            "Warning: column " + '"' + header + '"'
+            " from " + input_file + " will be ignored.",
+            file=sys.stderr,
+        )
     header_index = header_index + 1
 for required_header in xslx_col_index:
     if xslx_col_index[required_header] == -1:
@@ -261,11 +289,11 @@ for row in xslx_file.active.iter_rows(
     max_row=xslx_file.active.max_row,
     min_col=xslx_min_col,
     max_col=xslx_max_col,
-    values_only=True
+    values_only=True,
 ):
     row_dict = {}
     for csv_col in AXELOR_CSV_COLUMNS:
-        row_dict[csv_col] = ''
+        row_dict[csv_col] = ""
     col_index = xslx_min_col
     for cell in row:
         if col_index == xslx_col_index["name"]:
@@ -279,40 +307,52 @@ for row in xslx_file.active.iter_rows(
             for prod_cat in AXELOR_PRODUCT_CATEGORIES:
                 if prod_cat == cell:
                     valid_cat = True
-                    row_dict["productCategory_importId"] = AXELOR_PRODUCT_CATEGORIES[cell]
+                    row_dict["productCategory_importId"] = AXELOR_PRODUCT_CATEGORIES[
+                        cell
+                    ]
                     break
             for prod_fam in AXELOR_PRODUCT_FAMILIES:
                 if prod_fam == cell:
                     valid_fam = True
                     row_dict["productFamily_importId"] = AXELOR_PRODUCT_FAMILIES[cell]
                     break
-            code_incr = ''
-            ax_code_str = ''
+            code_incr = ""
+            ax_code_str = ""
             if valid_fam:
-                ax_code_str = AXELOR_FAMILY_SHORTHAND.get(cell, cell.upper().replace(" ", "_"))
+                ax_code_str = AXELOR_FAMILY_SHORTHAND.get(
+                    cell, cell.upper().replace(" ", "_")
+                )
             elif valid_cat:
-                ax_code_str = AXELOR_CATEGORY_SHORTHAND.get(cell, cell.upper().replace(" ", "_"))
+                ax_code_str = AXELOR_CATEGORY_SHORTHAND.get(
+                    cell, cell.upper().replace(" ", "_")
+                )
             else:
                 ax_code_str = cell.upper().replace(" ", "_")
             code_incr = cat_incr.get(ax_code_str, "0000")
             cat_incr[ax_code_str] = code_incr
             row_dict["code"] = ax_code_str + "-" + code_incr
             if not valid_fam:
-                row_dict["productFamily_importId"] = AXELOR_PRODUCT_FAMILIES[default_family]
+                row_dict["productFamily_importId"] = AXELOR_PRODUCT_FAMILIES[
+                    default_family
+                ]
             if not valid_cat:
-                row_dict["productCategory_importId"] = AXELOR_PRODUCT_CATEGORIES[default_category]
+                row_dict["productCategory_importId"] = AXELOR_PRODUCT_CATEGORIES[
+                    default_category
+                ]
             # Increment counter.
             code_incr_int = int(code_incr)
             code_incr_int = code_incr_int + 1
             if code_incr_int > 10:
-                cat_incr[ax_code_str] = "".join('0' + '0' + '0' + str(code_incr_int))
+                cat_incr[ax_code_str] = "".join("0" + "0" + "0" + str(code_incr_int))
             elif code_incr_int > 100:
-                cat_incr[ax_code_str] = "".join('0' + '0' + str(code_incr_int))
+                cat_incr[ax_code_str] = "".join("0" + "0" + str(code_incr_int))
             elif code_incr_int > 1000:
-                cat_incr[ax_code_str] = "".join('0' + str(code_incr_int))
+                cat_incr[ax_code_str] = "".join("0" + str(code_incr_int))
             else:
                 cat_incr[ax_code_str] = str(code_incr_int)
-        elif (col_index == xslx_col_index["sales_UOM"]) or (col_index == xslx_col_index["purchase_UOM"]):
+        elif (col_index == xslx_col_index["sales_UOM"]) or (
+            col_index == xslx_col_index["purchase_UOM"]
+        ):
             unit_id = -1
             for unit in AXELOR_UNITS:
                 # These sections might contain int,
@@ -327,11 +367,11 @@ for row in xslx_file.active.iter_rows(
                     if char in string.digits:
                         digit_loc = string_index
                     string_index = string_index + 1
-                if (digit_loc != -1) and (digit_loc != (len(str(cell))-1)):
+                if (digit_loc != -1) and (digit_loc != (len(str(cell)) - 1)):
                     # Test for a shortened unit form.
                     short_incr = 1
                     test_str = []
-                    while ( (short_incr + string_index) < len(str(cell)) ):
+                    while (short_incr + string_index) < len(str(cell)):
                         test_str += str(cell)[string_index + short_incr]
                         short_incr = short_incr + 1
                     test_str = "".join(test_str)
@@ -351,7 +391,7 @@ for row in xslx_file.active.iter_rows(
         col_index = col_index + 1
     # Add values not directly based on XSLX file.
     row_dict["productTypeSelect"] = default_type
-    row_dict["fullName"] = '[' + row_dict["code"] + ']' + " " + row_dict["name"]
+    row_dict["fullName"] = "[" + row_dict["code"] + "]" + " " + row_dict["name"]
 
     csv_out.writerow(row_dict.values())
     row_dict.clear()
